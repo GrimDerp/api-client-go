2dff579af4ccdf2143769d01d87e8fd0dd9ae0b6
Nam Nguyen <namn@bluemoon.com.vn>
2014-06-11 11:04:21 -0700

    Fix Travis build broken by last merge.
    Two problems:
    
      1. GOBIN was not in PATH.
      2. Decryption key was not set.

--- cli_test.sh
+++ cli_test.sh
@@ -3,17 +3,19 @@
 set -o nounset
 set -o errexit
 
-./api-client-go help 2>&1 | grep 'Reads functions'
-./api-client-go help 2>&1 | grep readsets
+export PATH=$HOME/gopath/bin:$PATH
+
+api-client-go help 2>&1 | grep 'Reads functions'
+api-client-go help 2>&1 | grep readsets
 
 openssl aes-256-ctr -d -a -in oauth_for_travis.pem.enc -out oauth_for_travis.pem -pass "pass:$KEY_PASSWORD"
 
-./api-client-go readsets search --use-oauth=oauth_for_travis.json --dataset-ids=376902546192 | grep ChhDSkRta1luOENoQ2otTmJNb2UzaWxiWUI=
+api-client-go readsets search --use-oauth=oauth_for_travis.json --dataset-ids=376902546192 | grep ChhDSkRta1luOENoQ2otTmJNb2UzaWxiWUI=
 
-./api-client-go readsets search --use-oauth=oauth_for_travis.json --dataset-ids=376902546192 --page-token=ChhDSkRta1luOENoQ2otTmJNb2UzaWxiWUI= | grep "Page Token"
+api-client-go readsets search --use-oauth=oauth_for_travis.json --dataset-ids=376902546192 --page-token=ChhDSkRta1luOENoQ2otTmJNb2UzaWxiWUI= | grep "Page Token"
 
-./api-client-go readsets search --use-oauth=oauth_for_travis.json --dataset-ids=376902546192 HG00310 | grep CJDmkYn8ChCj-NbMoe3ilbYB
+api-client-go readsets search --use-oauth=oauth_for_travis.json --dataset-ids=376902546192 HG00310 | grep CJDmkYn8ChCj-NbMoe3ilbYB
 
-./api-client-go readsets get --use-oauth=oauth_for_travis.json CJDmkYn8ChCj-NbMoe3ilbYB | grep HG00310
+api-client-go readsets get --use-oauth=oauth_for_travis.json CJDmkYn8ChCj-NbMoe3ilbYB | grep HG00310
 
-./api-client-go reads search --use-oauth=oauth_for_travis.json --readset-ids=CJDmkYn8ChCj-NbMoe3ilbYB | grep "Page Token"
+api-client-go reads search --use-oauth=oauth_for_travis.json --readset-ids=CJDmkYn8ChCj-NbMoe3ilbYB | grep "Page Token"
